f39f5128efece6b7cd889204e92ce524887dd0fa
Yusuf Zeren <ysfzrn@gmail.com>
2018-09-10 15:34:04 +0300

    Update RNViewOverflowManager
    Setting borderRadius with style object

--- android/src/main/java/com/entria/views/RNViewOverflowManager.java
+++ android/src/main/java/com/entria/views/RNViewOverflowManager.java
@@ -2,6 +2,7 @@ package com.entria.views;
 
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.ViewGroupManager;
+import com.facebook.react.uimanager.annotations.ReactProp;
 
 public class RNViewOverflowManager extends ViewGroupManager<RNViewOverflowLayout> {
 
@@ -15,6 +16,11 @@ public class RNViewOverflowManager extends ViewGroupManager<RNViewOverflowLayout
     @Override
     public String getName() {
         return REACT_CLASS;
+    }
+    
+    @ReactProp(name = "borderRadius", defaultFloat = 0f)
+    public void setBorderRadius(RNViewOverflowLayout view, float borderRadius) {
+      view.setBorderRadius(borderRadius*3.5f);
     }
 
 }
